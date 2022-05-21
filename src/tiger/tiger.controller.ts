a070df260016c702bf6884cf0441425796130e52
alan <843349919@qq.com>
2022-05-21 17:41:43 +0800

    异常过滤器v1

--- src/tiger/tiger.controller.ts
+++ src/tiger/tiger.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpException, HttpStatus, Get } from '@nestjs/common';
+import { ForbiddenException } from "../exceptions/forbidden.exception";
 
 @Controller('tiger')
-export class TigerController {}
+export class TigerController {
+  @Get()
+  async findAll() {
+    // throw new HttpException('Forbidden', HttpStatus.FORBIDDEN)
+    // throw new HttpException({
+    //   statue: HttpStatus.FORBIDDEN,
+    //   error: 'this is a custom message'
+    // }, HttpStatus.FORBIDDEN);
+    throw new ForbiddenException();
+  }
+}
