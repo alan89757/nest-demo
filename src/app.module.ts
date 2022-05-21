2f82f1caa712206812e2ce7e188e2dbb5ac4f879
alan <843349919@qq.com>
2022-05-21 17:11:14 +0800

    中间件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
@@ -9,10 +9,19 @@ import { DogsModule } from './dogs/dogs.module';
 import { TigerService } from './tiger/tiger.service';
 import { TigerModule } from './tiger/tiger.module';
 import { DatabaseModule } from './database/database.module';
+import { LoggerMiddleware } from "./common/logger.middleware";
 
 @Module({
   imports: [CatsModule, HttpModule, DogsModule, TigerModule, DatabaseModule],
   controllers: [AppController, CatsController],
   providers: [AppService, HttpService, TigerService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware, LoggerMiddleware).exclude(
+      { path: 'cats', method: RequestMethod.GET },
+      { path: 'cats', method: RequestMethod.POST },
+      'cats/(.*)',
+    ).forRoutes(CatsController)
+  }
+}
